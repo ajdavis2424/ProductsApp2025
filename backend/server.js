8e3ecb6065375bb32d156d5590d5d960a4528d13
ALexander Davis <ajdavis2424@gmail.com>
2025-01-15 17:56:16 -0500

    POST call now works. cleaned up console logs

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from '../models/product.model.js';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json()); //middleware allow us to accept json in req.body
+
 //Products route
-app.get('/products', (req, res) => {});
-console.log(process.env.MONGO_URI);
+app.post('/api/products', async (req, res) => {
+  //get product from user (body)
+  const product = req.body;
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Please fill in all required fields!' });
+  }
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save(); //-- Save to DB!
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error('Error creating a new product:', error.message);
+    res.status(500).json({ success: false, message: 'Server Error' });
+  }
+});
 
 app.listen(5005, () => {
   connectDB();
